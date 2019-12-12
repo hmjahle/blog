54dad079e29bc5dcc3c8b410cce9d74553aae175
hmjahle <hansmarkusjahle@gmail.com>
2019-12-13 00:14:59 +0100

    Created Create Content
    Page to create content

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,41 @@
+import React, {useContext, useState} from 'react';
+import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
+
+const CreateScreen = ({navigation}) => {
+    const [title, setTitle]= useState('');
+    const [content, setContent] = useState('');
+
+    return (
+        <View>
+            <Text style={styles.label}> Enter Title:</Text>
+            <TextInput value={title}
+                        style={styles.input}
+                        onChangeText={(text) => setTitle(text)}/>
+            <Text style={styles.label}>Enter Content:</Text>
+            <TextInput value={content}
+                        style={styles.input}
+                        onChangeText={(text) => setContent(text)}/>
+            <Button title="Add Blog Post" />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 15,
+        padding: 5,
+        margin: 5
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5
+
+    }
+
+});
+
+export default CreateScreen;
